8e3a304ea3fa607bbc332415a2441bcb036e443a
mykolalaudspeaker31 <mykola.laudspeaker31@gmail.com>
2023-03-27 17:30:45 +0300

    Feature: base setup

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'prettier'],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
+  root: true,
+};
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+console.log('Hello World!');
+
+/**
+ * Prints hello world
+ * @param param ...
+ */
+export function hello(param: string) {
+  console.log('hello world');
+}
